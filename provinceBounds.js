0cf6c7fe5a3137016f43d9a8e5993811a859907e
richmondpepito <richmondpepito@gmail.com>
2025-05-05 11:26:37 +0800

    Add files via upload

---
+++ provinceBounds.js
@@ -0,0 +1,6 @@
+const provinceBounds = {
+  "Ilocos Norte": [[18.1, 120.5], [18.6, 121.0]],
+  "Ilocos Sur": [[17.0, 120.3], [17.9, 120.8]],
+  "La Union": [[16.3, 120.2], [16.8, 120.6]]
+  // Add remaining provinces...
+};
